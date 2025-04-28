c63c31ca26294528f1b53f7c88f4f11a5f0f3e70
ZotRoma <zotov.roman.2001@gmail.com>
2025-04-28 16:07:44 +0400

    add time metka

--- lab2/server/server.py
+++ lab2/server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request
 import cv2
 import numpy as np
+import time
 
 app = Flask(__name__)
 
@@ -12,12 +13,15 @@ def upload():
     global last_frame
     # Получение JPEG-кадра от клиента
     data = request.data
+    # Извлечение временной метки из заголовка (если нет, используется текущее время)
+    timestamp = request.headers.get('Timestamp', str(time.time()))
     nparr = np.frombuffer(data, np.uint8)
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     # Обработка кадра (выделение контуров)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 100, 200)
-    #median = cv2.medianBlur(frame, 3)
+    # Наложение временной метки на изображение
+    cv2.putText(edges, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     # Сжатие обработанного кадра в JPEG
     last_frame = cv2.imencode('.jpg', edges)[1].tobytes()
     return 'OK'
